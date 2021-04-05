fff5ad8a39a04940aa20a6e3c559216be7f127f3
gavin-ren0707 <gavin.ren@dasheng.com.tw>
2021-04-05 21:45:11 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class InfoComponent  {
+  name = 'Angular ' + VERSION.major;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+}
